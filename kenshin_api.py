679362c103e10f14bf6c633b1a29fcf7958e7bb3
zzl0 <zhuzhaolong0@gmail.com>
2016-08-23 13:48:52 +0800

    Init

---
+++ kenshin_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class KenshinLeafNode(LeafNode):
+    pass
+
+
+class KenshinReader(object):
+    pass
+
+
+class KenshinFinder(object):
+    pass
+
+
+if __name__ == '__main__':
+    pass
